49f53303e55459bbd7dd565be96cb0361ed5a37b
lechev95 <lecho.lechev@gmail.com>
2024-03-12 16:52:09 +0200

    add test for all links

---
+++ tests/ui.test.js
@@ -0,0 +1,9 @@
+const { expect, test } = require('@playwright/test');
+
+test("Verify All Books link is available", async ( { page } ) => {
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector("nav.navbar");
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isLinkVisible = await allBooksLink.isVisible();
+    expect(isLinkVisible).toBe(true);
+});
